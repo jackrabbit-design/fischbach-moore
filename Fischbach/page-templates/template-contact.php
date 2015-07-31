e93e42a402177b8a6cf713e496bd458a0d3f5db9
kcroakeJRD <kevin@jumpingjackrabbit.com>
2015-07-31 09:38:47 -0400

    Contact page Creation

---
+++ Fischbach/page-templates/template-contact.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Contact
+ *
+ * @package WordPress
+ * @subpackage Frischbach
+ */
+
+get_header();
+$left_img1 = get_field('left_featured_image');
+$right_img1 = get_field('right_featured_image');
+ ?>
+
+	<div id="pg-banner">
+    	<div class="container">
+        	<h2><?php the_title(); ?></h2>
+        </div>
+        
+        <!--NOTE 
+        	remove banner-margin-true class if you dont need space between banners 
+            -->
+        <div class="banner-wrap clearfix">
+        	<div class="banner-left pull-left" style="background-image:url(<?php echo $left_img1; ?>)"></div>
+            <div class="banner-right pull-right banner-margin-true">
+            	<div class="banner-right-bg" style="background-image:url(<?php echo $right_img1; ?>)"></div>
+            </div>
+        </div>
+    </div><!--pg-banner/-->
+
+        <section id="content-wrap">
+    	<div class="container">
+        	<div class="container-inner clearfix">
+            	<article id="article" class="main-content pull-left">
+                	<?php 
+						while ( have_posts() ) : the_post();
+						the_content(); 
+						endwhile;
+						?>
+                </article>
+                
+                <aside id="side-bar" class="pull-right">
+
+                </aside>
+
+
+                           </div>
+        </div>
+    </section>
+
+    <?php get_footer(); ?>
